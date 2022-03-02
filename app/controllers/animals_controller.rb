5b07464d0e17d3bc626ab9855a84ca2b6db7be90
Amanda Amorim <amandagadelhaamorim@gmail.com>
2022-03-02 17:19:32 -0300

    edit, update, show e delete

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -18,6 +18,31 @@ class AnimalsController < ApplicationController
     end
   end
 
+  def show
+    @animal = Animal.find(params[:id])
+  end
+
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    @animal.update(animal_params)
+    if @animal.save
+      redirect_to animal_path(@animal)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+
+    redirect_to animals_path
+  end
+
   private
 
   def animal_params
