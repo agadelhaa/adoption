576796bc937db5f10d67fa83f4c02b53fcc572a9
Amanda Amorim <amandagadelhaamorim@gmail.com>
2022-03-03 14:01:27 -0300

    view adicionada

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,3 +1,15 @@
 class OrdersController < ApplicationController
-  
+  def edit
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    if @order.save
+      redirect_to animal_path(@order.animal)
+    else
+      render :edit
+    end
+  end
 end
