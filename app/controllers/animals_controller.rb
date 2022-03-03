5f89db26d2681dbc0593cc5728e873668bee8f7b
Luiz Fernando Alvarenga <luizfca7@gmail.com>
2022-03-03 16:08:52 -0300

    ANIMAL PHOTOS

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -12,7 +12,7 @@ class AnimalsController < ApplicationController
 
     @animal = Animal.new(animal_params)
     @animal.user = current_user
-   
+
     if @animal.save
       redirect_to animals_path, notice: 'animal was successfully created.'
     else
@@ -48,6 +48,6 @@ class AnimalsController < ApplicationController
   private
 
   def animal_params
-    params.require(:animal).permit(:name, :gender, :age, :size, :specie, :description, :photo)
+    params.require(:animal).permit(:name, :gender, :age, :size, :specie, :description, photos: [])
   end
 end
