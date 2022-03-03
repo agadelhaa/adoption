5843c12b213a09c30e778779b421b74611ed4eb1
Luiz Fernando Alvarenga <luizfca7@gmail.com>
2022-03-03 13:50:04 -0300

    ORDER CONTROLLER

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,25 @@
 class OrdersController < ApplicationController
+  def new
+    @user_id = current_user.id
+    @animal = Animal.find(params[:animal_id])
+    @order = Order.new
+  end
+
+  def create
+    @animal = Animal.find(params[:animal_id])
+    @user_id = current_user.id
+    @order = Order.new(order_params)
+    @order.animal = @animal
+    if @order.save
+      redirect_to animal_path(@animal)
+    else
+      render 'animal/show'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:adopted, :user_id, :animal_id)
+  end
 end
